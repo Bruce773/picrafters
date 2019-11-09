903ef28730ce54c18cd92e12c87b72ec3dd0fdb5
Bruce Johnson <bruce@yogainternational.com>
2019-11-09 10:34:24 -0500

    adds form control and types

--- src/RegisterPage/RegisterPage.tsx
+++ src/RegisterPage/RegisterPage.tsx
@@ -1,8 +1,42 @@
-import React from "react";
-import Input from "@material-ui/core";
+import React, { useState, Dispatch, SetStateAction } from "react";
+import Input from "@material-ui/core/Input";
+import { lightGrey, darkGrey } from "../colors";
 
-export const RegisterPage: React.FC = () => (
-  <>
-    {/* use https://mailchimp.com/developer/guides/manage-subscribers-with-the-mailchimp-api/ for form API*/}
-  </>
-);
+export const RegisterPage: React.FC = () => {
+  const [studentsName, setStudentsName] = useState("");
+
+  interface FormFieldsTypes {
+    updateValue: Dispatch<SetStateAction<string>>;
+    value: string;
+    label: string;
+  }
+
+  const formFields: FormFieldsTypes[] = [
+    {
+      label: `Student's Name`,
+      value: studentsName,
+      updateValue: setStudentsName
+    }
+  ];
+
+  return (
+    <>
+      {/* use https://mailchimp.com/developer/guides/manage-subscribers-with-the-mailchimp-api/ for form API*/}
+      {formFields.map(({ label, value, updateValue }) => (
+        <Input
+          onChange={({ target: { value } }) => updateValue(value)}
+          value={value}
+          placeholder={label}
+          disableUnderline
+          style={{
+            padding: "6px 16px",
+            borderRadius: "6px",
+            backgroundColor: lightGrey,
+            color: darkGrey,
+            fontSize: "24px"
+          }}
+        />
+      ))}
+    </>
+  );
+};
--- src/colors.ts
+++ src/colors.ts
@@ -2,3 +2,4 @@ export const gold = `#df9a50`;
 export const blue = `#9ac9cf`;
 export const brightBlue = `#00b6ce`;
 export const darkGrey = `#737373e0`;
+export const lightGrey = `#c7c7c742`;
