9ba06d57af6dcff4f137fc99cce20b84af0d644c
Bruce Johnson <bruce@yogainternational.com>
2019-11-09 09:15:52 -0500

    adds types for RegisterPage

--- src/RegisterPage/RegisterPage.tsx
+++ src/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Input from "@material-ui/core";
 
-export const RegisterPage = () => (
+export const RegisterPage: React.FC = () => (
   <>
     {/* use https://mailchimp.com/developer/guides/manage-subscribers-with-the-mailchimp-api/ for form API*/}
   </>
