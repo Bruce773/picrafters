2490b4cebebbb0aba6d5ba3259cdc36c079e6701
Bruce Johnson <bruce@yogainternational.com>
2019-11-15 22:09:37 -0500

    adds OG tags and updates Footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -2,19 +2,35 @@ import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
+import FacebookIcon from "@material-ui/icons/Facebook";
 import React from "react";
-import { gold } from "../colors";
+import { brightBlue, gold } from "../colors";
+import { Link } from "../GlobalComponents";
 
 export const Footer = () => {
   const currentDate = new Date();
 
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="lg" style={{ marginBottom: "15px" }}>
       <Box style={{ textAlign: "center", paddingTop: "20px" }}>
         <Divider />
-        <Typography style={{ fontSize: "2vh", color: gold, padding: "20px" }}>
+        <Typography
+          style={{
+            fontSize: "2vh",
+            color: gold,
+            padding: "20px",
+            paddingBottom: "0px"
+          }}
+        >
           Copyright {currentDate.getFullYear()} © Pi Crafters
         </Typography>
+        <a
+          style={{ display: "inline", color: brightBlue }}
+          href="https://www.facebook.com/picrafters4u/"
+          target="_blank"
+        >
+          <FacebookIcon color="inherit" fontSize="large" />
+        </a>
       </Box>
     </Container>
   );
